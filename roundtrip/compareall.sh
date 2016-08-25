ffecfb0d80f68010e95db523bd1f3bab537272f5
Milos Sramek <msramek22@gmail.com>
2016-08-25 14:59:56 +0200

    modified compareall.sh to process specific directories

--- roundtrip/compareall.sh
+++ roundtrip/compareall.sh
@@ -3,6 +3,13 @@
 
 . config.sh
 
+function usage
+{
+	echo "$0: Compare pdf files and generate pair pdfs" 1>&2
+	echo "Usage: $0 applist" 1>&2
+	echo "If applist is not specified, all applications specified in config.sh will be processed." 1>&2
+}
+
 function cmp ()
 {
 	#echo 1 $1
@@ -33,13 +40,42 @@ function cmp ()
 	fi
 }
 
+# read the options
+TEMP=`getopt -o h --long help -n 'test.sh' -- "$@"`
+eval set -- "$TEMP"
+
+# extract options and their arguments into variables.
+while true ; do
+    case "$1" in
+        -h|--help) 
+		usage; exit 1;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
+shift $(expr $OPTIND - 1 )
+
 #get list of source pdfs. We assume the same structure and files in the targer directories
 cd $sourcedir
 pdfs=`find . -name \*.pdf|grep -v pair|sort -n -k 1.7,1.9`
 cd ..
 echo $pdfs
 
-#echo $pdfs
-for app in `echo $rtripapps`; do
-	for pdfdoc in $pdfs; do cmp `basename $pdfdoc` $app; done
-done
+if [[ $# -gt 0 ]] 
+then
+	while test $# -gt 0; 
+	do
+		if [ -d "$1" ]; then
+  			echo Processing $1
+			for pdfdoc in $pdfs; do cmp `basename $pdfdoc` $1; done
+		else
+  			echo Directory $1 does not exist
+		fi
+  		shift
+	done
+else
+	for app in `echo $rtripapps`; do
+  		echo Processing $app
+		for pdfdoc in $pdfs; do cmp `basename $pdfdoc` $app; done
+	done
+fi
